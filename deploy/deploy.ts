03732a9e3cf1d42832ef6eb7f658671a0b1ab380
wangjie02 <gxbbrvht04336@outlook.com>
2025-12-26 10:20:23 +0800

    Update deploy script for BallotGuard

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,16 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const optionCounts = [3, 4, 3, 2, 4];
+
+  const deployedSurvey = await deploy("BallotGuard", {
     from: deployer,
+    args: [optionCounts],
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`BallotGuard contract: `, deployedSurvey.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_ballotGuard"; // id required to prevent reexecution
+func.tags = ["BallotGuard"];
